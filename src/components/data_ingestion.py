4a6d2fa415e2ac5ad8c66a888f5a1be81431f5ad
Yogesh3454 <yogeshgunjal75@gmail.com>
2024-10-26 11:34:53 +0530

    modify

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,4 +1,5 @@
-import numpy as pd
+import numpy as np
+import pandas as pd
 import os
 import sys
 from src.exception import CustomException
@@ -25,7 +26,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook\student.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
